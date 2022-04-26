1b554a9d7ed63daf1d850665145c813451b73bc3
Pandra <kassavndra@gmail.com>
2022-04-27 01:36:44 +0900

    modify profile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:create_app() --log-file -
+web: gunicorn server:app --log-file -
--- server.py
+++ server.py
@@ -2,4 +2,5 @@ import os
 from quattro_scelte import create_app
 
 if __name__ == '__main__':
-    create_app().run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app = create_app()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
