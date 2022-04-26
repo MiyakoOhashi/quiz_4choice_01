3ce6e36c355e0614011d7b27742ecb5e63527009
Pandra <kassavndra@gmail.com>
2022-04-26 23:30:42 +0900

    first commit

---
+++ quattro_scelte/config.py
@@ -0,0 +1,9 @@
+# quattro_scelte/config.py        2022/04/25  M.O
+
+SQLALCHEMY_DATABASE_URI = ''
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# os.urandom(24)
+SECRET_KEY = b'81\xf2\xd3u^\x8f\xc6I\x93\xa5\x18\\7\x14\xac\xddv\xbbd{V^\x86'
+
+DEBUG = False
